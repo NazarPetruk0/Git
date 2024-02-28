7b8625c6528d56f9941e7880179af1220e04692d
NazarPetruk0 <154916004+NazarPetruk0@users.noreply.github.com>
2024-02-29 01:47:21 +0200

    Update task3.py

--- завдання3.py
+++ завдання3.py
@@ -1,11 +1,4 @@
 import re
-
-def normalize_phone(phone_number):
-    cleaned_number = re.sub(r'\D', '', phone_number)
-    if not cleaned_number.startswith('+'):
-        cleaned_number = '+38' + cleaned_number.lstrip('38')
-    return cleaned_number
-
 raw_numbers = [
     "067\\t123 4567",
     "(095) 234-5678\\n",
@@ -17,6 +10,20 @@ raw_numbers = [
     "38050-111-22-22",
     "38050 111 22 11   ",
 ]
+dirty_phones=["    +38(050)123-32-34",
+"     0503451234",
+"(050)8889900",
+"38050-111-22-22",
+"38050 111 22 11   "]
+def normalize_phone(phone_number):
+    p1=r"[\d\+]+"
+    phone_number=''.join(re.findall(p1,phone_number))
+    if len(phone_number)==10:
+        phone_number='+38'+phone_number
+    elif len(phone_number)==12:
+        phone_number='+'+phone_number
+    return phone_number
 
-sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
-print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
+print("Нормалізовані номери телефонів для SMS-розсилки: ")
+for phone in raw_numbers:
+    print(normalize_phone(phone))
