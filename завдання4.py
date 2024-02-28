ac7c29079d140638f2766ba9b5614e3bb8ad4612
NazarPetruk0 <154916004+NazarPetruk0@users.noreply.github.com>
2024-02-29 01:36:06 +0200

    Update task4.py

--- завдання4.py
+++ завдання4.py
@@ -1,30 +1,29 @@
-from datetime import datetime as dtdt, timedelta
+import datetime as dt
+from datetime import datetime as dtdt
 
-def get_upcoming_birthdays(users):
-    today = dtdt.today().date()
-    upcoming_birthdays = []
+def get_upcoming_birthdays(users=None):
+    tdate=dtdt.today().date()
+    birthdays=[]
     for user in users:
-        birthday = dtdt.strptime(user["birthday"], "%Y.%m.%d").date().replace(year=today.year)
-        if birthday < today:
-            birthday = birthday.replace(year=today.year + 1)
-        days_until_birthday = (birthday - today).days
-        congratulation_date = birthday
-        if days_until_birthday > 7:
-            continue
-        elif days_until_birthday == 0:
-            congratulation_date = today
-        elif days_until_birthday < 0:
-            continue
-        elif today.weekday() == 5: 
-            congratulation_date += timedelta(days=2)
-        elif today.weekday() == 6:
-            congratulation_date += timedelta(days=1)
-        upcoming_birthdays.append({"name": user["name"], "congratulation_date": congratulation_date.strftime("%Y.%m.%d")})
-    return upcoming_birthdays
+        bdate=user["birthday"] 
+        bdate=str(tdate.year)+bdate[4:] 
+        bdate=dtdt.strptime(bdate, "%Y.%m.%d").date() 
+        week_day=bdate.isoweekday() 
+        days_between=(bdate-tdate).days
+        if 0<=days_between<7: 
+            if week_day<6: 
+                birthdays.append({'name':user['name'], 'birthday':bdate.strftime("%Y.%m.%d")})
+            else:
+                if (bdate+dt.timedelta(days=1)).weekday()==0:
+                    birthdays.append({'name':user['name'], 'birthday':(bdate+dt.timedelta(days=1)).strftime("%Y.%m.%d")})
+                elif (bdate+dt.timedelta(days=2)).weekday()==0: 
+                    birthdays.append({'name':user['name'], 'birthday':(bdate+dt.timedelta(days=2)).strftime("%Y.%m.%d")})
+    return birthdays
 
 users = [
-    {"name": "John Doe", "birthday": "1985.01.23"},
-    {"name": "Jane Smith", "birthday": "1990.01.28"}
+   {'name': 'Jake Smith', 'birthday': '1990.03.02'},
+{'name': 'John Doe', 'birthday': '1985.03.05'},
+{'name': 'Ethan Williams', 'birthday': '1970.02.29'},
+{'name': 'Smith Smith', 'birthday': '1990.03.06'}
 ]
-upcoming_birthdays = get_upcoming_birthdays(users)
-print("Список привітань на цьому тижні:", upcoming_birthdays)
+print(get_upcoming_birthdays(users))
