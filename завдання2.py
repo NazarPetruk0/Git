7005159fdea1dd56fa219eefa33a8b596f131f8f
NazarPetruk0 <154916004+NazarPetruk0@users.noreply.github.com>
2024-02-29 02:07:00 +0200

    Update task2.py

--- завдання2.py
+++ завдання2.py
@@ -1,12 +1,12 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    if not(1 <= min <= max <= 1000) or quantity < 1:
+    if not(1 <= min <= max <= 1000) or quantity < 1 or quantity > (max - min + 1):
         return []
     unique_nums = set()
     while len(unique_nums) < quantity:
         unique_nums.add(random.randint(min, max))
-    return sorted(list(unique_nums))
+    return sorted(unique_nums)  
 
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+lottery_numbers = get_numbers_ticket(10, 14, 5)
 print("Ваші лотерейні числа:", lottery_numbers)
